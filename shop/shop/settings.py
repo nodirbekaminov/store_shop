83c6a186f10449ba266059a84564d18310f5945e
Nodir <nodiraminov0101@gmail.com>
2026-01-13 12:54:36 +0500

    Initial commit

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -11,13 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
